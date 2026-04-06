492b82cfd6cd35a76fe6eb7a928b4daa0a552f41
Yonatan-Ethiopia <yonatanethiopia1@gmail.com>
2026-04-06 10:18:53 +0300

    v1 basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli{
+	ip: Option<String>,
+	port: Option<String>,
+	
+	#[arg(short, long)]
+	persist:bool,
+	
+	#[arg(short, long)]
+	clear:bool
+}
+
+fn main(){
+	let args = Cli::parse();
+	
+	if args.clear{
+		print!("unset http_proxy; unset https_proxy;");
+	}
+	else if let( Some(ip), Some(port)) = (args.ip, args.port){
+		let proxy_str = format!("http://{}:{}", ip, port);
+		print!("export http_proxy=\"{}\"; export https_proxy=\"{}\";", proxy_str, proxy_str);
+	}
 }
